ecd458c28f9bbfea059d51f95cd9a964e01f243b
vmadmin <vijaykr391807@gmail.com>
2020-09-25 23:18:25 +0530

    added search methods in controller

--- src/main/java/com/mindtree/shoppingcart/controller/ShoppingCartController.java
+++ src/main/java/com/mindtree/shoppingcart/controller/ShoppingCartController.java
@@ -18,39 +18,46 @@ import com.mindtree.shoppingcart.service.ShoppingCartServiceImpl;
 @RequestMapping("/shopping-cart")
 public class ShoppingCartController {
 
-	//@Autowired
-	//ShoppingCartServiceImpl shoppingCartService;
+	@Autowired
+	ShoppingCartServiceImpl shoppingCartService;
 
-	/*
-	 * // Search Product By ID
-	 * 
-	 * @RequestMapping(value = "/searchProductById/{id}", method =
-	 * RequestMethod.GET) public ResponseEntity<?>
-	 * searchProductById(@PathVariable("id") int id) { try { List<Object> response =
-	 * null; response = shoppingCartService.searchProductById(id); return new
-	 * ResponseEntity<>(response, HttpStatus.OK); } catch
-	 * (ShoppingCartServiceException e) { return new ResponseEntity<>("Exception" +
-	 * e.getLocalizedMessage(), HttpStatus.BAD_REQUEST); } }
-	 * 
-	 * // Search Product By Name
-	 * 
-	 * @RequestMapping(value = "/searchProductByName/{name}", method =
-	 * RequestMethod.GET) public ResponseEntity<?>
-	 * searchProductByName(@PathVariable("name") String name) { try { List<Object>
-	 * response = null; response = shoppingCartService.searchProductByName(name);
-	 * return new ResponseEntity<>(response, HttpStatus.OK); } catch
-	 * (ShoppingCartServiceException e) { return new ResponseEntity<>("Exception" +
-	 * e.getLocalizedMessage(), HttpStatus.BAD_REQUEST); } }
-	 * 
-	 * // Search Product By Category
-	 * 
-	 * @RequestMapping(value = "/searchProductByCategory/{category}", method =
-	 * RequestMethod.GET) public ResponseEntity<?>
-	 * searchProductByCategory(@PathVariable("category") String category) { try {
-	 * List<Object> response = null; response =
-	 * shoppingCartService.searchProductByCategory(category); return new
-	 * ResponseEntity<>(response, HttpStatus.OK); } catch
-	 * (ShoppingCartServiceException e) { return new ResponseEntity<>("Exception" +
-	 * e.getLocalizedMessage(), HttpStatus.BAD_REQUEST); } }
-	 */
+	// Search Product By ID
+
+	@RequestMapping(value = "/searchProductById/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> searchProductById(@PathVariable("id") int id) {
+		try {
+			List<Object> response = null;
+			response = shoppingCartService.searchProductById(id);
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (ShoppingCartServiceException e) {
+			return new ResponseEntity<>("Exception" + e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	// Search Product By Name
+
+	@RequestMapping(value = "/searchProductByName/{name}", method = RequestMethod.GET)
+	public ResponseEntity<?> searchProductByName(@PathVariable("name") String name) {
+		try {
+			List<Object> response = null;
+			response = shoppingCartService.searchProductByName(name);
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (ShoppingCartServiceException e) {
+			return new ResponseEntity<>("Exception" + e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	// Search Product By Category
+
+	@RequestMapping(value = "/searchProductByCategory/{category}", method = RequestMethod.GET)
+	public ResponseEntity<?> searchProductByCategory(@PathVariable("category") String category) {
+		try {
+			List<Object> response = null;
+			response = shoppingCartService.searchProductByCategory(category);
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (ShoppingCartServiceException e) {
+			return new ResponseEntity<>("Exception" + e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+
 }
